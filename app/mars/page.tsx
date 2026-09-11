8a001be7ef71d64b32fc200ac5ef5656a8a610dd
thomaspeterkueper <thomas.peter.kueper@gmail.com>
2026-09-11 08:14:47 +0200

    feat(mars): add Mars route

---
+++ app/mars/page.tsx
@@ -0,0 +1,9 @@
+import MarsRegionPreview from './MarsRegionPreview'
+
+export const metadata = {
+  title: 'NOXIA Mars',
+}
+
+export default function MarsPage() {
+  return <MarsRegionPreview />
+}
