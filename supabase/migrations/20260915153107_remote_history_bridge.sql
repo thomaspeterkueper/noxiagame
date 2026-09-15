33faa9958046b68ce1ea4edb98ed3184d927057f
thomaspeterkueper <thomas.peter.kueper@gmail.com>
2026-09-15 17:35:01 +0200

    chore(db): bridge hosted custody marketplace rollout

---
+++ supabase/migrations/20260915153107_remote_history_bridge.sql
@@ -0,0 +1,5 @@
+-- Remote migration-history bridge.
+-- Hosted NOXIA rolled out 20260911111420_custody_marketplace_core.sql
+-- as migration 20260915153107 (`custody_marketplace_core_manual_rollout`).
+
+select 1;
